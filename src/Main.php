3d15e28f7e3dd8d159001f1c3ef73c7fe82db91f
Jackso-n <108813841+Jackso-n@users.noreply.github.com>
2022-07-06 19:28:13 +0545

    Main.php

---
+++ src/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Gui;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\PluginBase;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+//Add To Work InvMenu
+use muqsit\invmenu\InvMenu;
+use muqsit\invmenu\InvMenuHandler;
+
+class Main extends PluginBase implements Listener{
+  //Enable Function
+  public function onEnable() : void
+{
+  if(!InvMenuHandler::isRegistered()){
+  InvMenuHandler::register($this);
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+}
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+  //Main Command
+if($command->getName() == "test"){
+  $sender->sendMessage("Test");
+  }
+return true;
+ }
+}
